c8d8d8d3ddc892ea7bdbedf54372dc7fcd637a7e
tesic-kawagoe <con@tes-ic.com>
2018-08-11 12:03:24 +0000

    ranking

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -24,6 +24,11 @@
                                 </div>
                             </div>
                         </div>
+                        @if (isset($item->count))
+                            <div class="panel-footer">
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Wants</p>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
